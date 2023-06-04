728014d51b70ceee8488e15e72bbdc2b8f4cca2e
ankitxcoding <ankitxcoding@gmail.com>
2023-06-04 21:07:58 +0530

    account login popup added

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// login form js
+var modal = document.getElementById('account');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
