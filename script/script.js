72c7ee152fb27ba7e9a14e94f93f2529f50cc76d
ankitxcoding <ankitxcoding@gmail.com>
2023-06-05 12:43:27 +0530

    camera option added

--- script/script.js
+++ script/script.js
@@ -1,7 +1,36 @@
 // login form js
+
 var modal = document.getElementById('account');
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+// camera access
+
+function openCam(){
+    let All_mediaDevices=navigator.mediaDevices
+    if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+       console.log("getUserMedia() not supported.");
+       return;
+    }
+    All_mediaDevices.getUserMedia({
+       audio: true,
+       video: true
+    })
+    .then(function(vidStream) {
+       var video = document.getElementById('videoCam');
+       if ("srcObject" in video) {
+          video.srcObject = vidStream;
+       } else {
+          video.src = window.URL.createObjectURL(vidStream);
+       }
+       video.onloadedmetadata = function(e) {
+          video.play();
+       };
+    })
+    .catch(function(e) {
+       console.log(e.name + ": " + e.message);
+    });
 }
