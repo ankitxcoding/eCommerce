95893e3c412bcdb026c3f4c1844e8d2ae321daab
ankitxcoding <ankitxcoding@gmail.com>
2023-06-06 12:00:29 +0530

    camera popup added with improvement

--- script/script.js
+++ script/script.js
@@ -1,43 +1,56 @@
 // login form js
 
-var modal = document.getElementById('account');
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+var modal = document.getElementById("account");
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 // camera access
 
-function openCam(){
-    let All_mediaDevices=navigator.mediaDevices
-    if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
-       console.log("getUserMedia() not supported.");
-       return;
-    }
-    All_mediaDevices.getUserMedia({
-       audio: true,
-       video: true
+function openCam() {
+  let All_mediaDevices = navigator.mediaDevices;
+  if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+    console.log("getUserMedia() not supported.");
+    return;
+  }
+
+  All_mediaDevices.getUserMedia({ audio: true, video: true })
+    .then(function (vidStream) {
+      var video = document.getElementById("videoCam");
+      if ("srcObject" in video) {
+        video.srcObject = vidStream;
+      } else {
+        video.src = window.URL.createObjectURL(vidStream);
+      }
+      video.onloadedmetadata = function (e) {
+        video.play();
+      };
+
+      document.getElementById("camera").style.display = "block";
     })
-    .then(function(vidStream) {
-       var video = document.getElementById('videoCam');
-       if ("srcObject" in video) {
-          video.srcObject = vidStream;
-       } else {
-          video.src = window.URL.createObjectURL(vidStream);
-       }
-       video.onloadedmetadata = function(e) {
-          video.play();
-       };
-    })
-    .catch(function(e) {
-       console.log(e.name + ": " + e.message);
+    .catch(function (e) {
+      console.log(e.name + ": " + e.message);
     });
 }
 
-var modal = document.getElementById('camera');
-window.onclick = function(event) {
-    if (event.target == webcam) {
-        modal.style.display = "none";
-    }
+function closeCam() {
+  var video = document.getElementById("videoCam");
+  if (video.srcObject) {
+    video.pause();
+    video.srcObject.getTracks().forEach(function (track) {
+      track.stop();
+    });
+    video.srcObject = null;
+  }
+
+  document.getElementById("camera").style.display = "none";
 }
+
+window.onclick = function (event) {
+  var camera = document.getElementById("camera");
+  if (event.target == camera) {
+    closeCam();
+  }
+};
