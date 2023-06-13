099b983151cd20f6f0cde7d6599aaeaab7500b9a
ankitxcoding <ankitxcoding@gmail.com>
2023-06-13 15:48:08 +0530

    wishlist functionality added

--- script/script.js
+++ script/script.js
@@ -70,78 +70,88 @@ $(document).ready(function() {
 
 // home page content
 
-let contentTitle;
+let CartArr = JSON.parse(localStorage.getItem("cart")) || [];
+let ContainerClothing = document.getElementById("containerClothing");
+let ContainerAccessories = document.getElementById("containerAccessories");
 
-console.log(document.cookie);
-function dynamicClothingSection(ob) {
-  let boxDiv = document.createElement("div");
-  boxDiv.id = "box";
-
-  let boxLink = document.createElement("a");
-
-  let imgTag = document.createElement("img");
-  imgTag.src = ob.preview;
-
-  let detailsDiv = document.createElement("div");
-  detailsDiv.id = "details";
-
-  let h3 = document.createElement("h3");
-  let h3Text = document.createTextNode(ob.name);
-  h3.appendChild(h3Text);
-
-  let h4 = document.createElement("h4");
-  let h4Text = document.createTextNode(ob.brand);
-  h4.appendChild(h4Text);
-
-  let h2 = document.createElement("h2");
-  let h2Text = document.createTextNode("rs  " + ob.price);
-  h2.appendChild(h2Text);
-
-  boxDiv.appendChild(boxLink);
-  boxLink.appendChild(imgTag);
-  boxLink.appendChild(detailsDiv);
-  detailsDiv.appendChild(h3);
-  detailsDiv.appendChild(h4);
-  detailsDiv.appendChild(h2);
-
-  return boxDiv;
+async function FetchData() {
+  try {
+    let res = await fetch("https://648050cdf061e6ec4d490d80.mockapi.io/product");
+    if (res.ok) {
+      let data = await res.json();
+      DisplayProducts(data);
+    } else {
+      console.log("Request failed with status:", res.status);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-let mainContainer = document.getElementById("mainContainer");
-let containerClothing = document.getElementById("containerClothing");
-let containerAccessories = document.getElementById("containerAccessories");
+FetchData();
 
-let httpRequest = new XMLHttpRequest();
+function DisplayProducts(data) {
+  ContainerClothing.innerHTML = "";
+  ContainerAccessories.innerHTML = "";
 
-httpRequest.onreadystatechange = function() {
-  if (this.readyState === 4) {
-    if (this.status == 200) {
-      contentTitle = JSON.parse(this.responseText);
-      if (document.cookie.indexOf(",counter=") >= 0) {
-        var counter = document.cookie.split(",")[1].split("=")[1];
-        document.getElementById("badge").innerHTML = counter;
+  data.forEach((product) => {
+    let boxDiv = document.createElement("div");
+    boxDiv.id = "box";
+
+    let boxLink = document.createElement("a");
+
+    let imgTag = document.createElement("img");
+    imgTag.src = product.preview;
+
+    let detailsDiv = document.createElement("div");
+    detailsDiv.id = "details";
+
+    let h3 = document.createElement("h3");
+    let h3Text = document.createTextNode(product.name);
+    h3.appendChild(h3Text);
+
+    let h4 = document.createElement("h4");
+    let h4Text = document.createTextNode(product.brand);
+    h4.appendChild(h4Text);
+
+    let h2 = document.createElement("h2");
+    let h2Text = document.createTextNode("Rs. " + product.price);
+    h2.appendChild(h2Text);
+
+    let add_to_cart = document.createElement("button");
+    add_to_cart.textContent = "Add to Cart";
+
+    add_to_cart.addEventListener("click", () => {
+      if (checkDuplicate(product)) {
+        alert("Product Already in Cart");
+      } else {
+        CartArr.push({ ...product, quantity: 1 });
+        localStorage.setItem("cart", JSON.stringify(CartArr));
+        alert("Product Added To Cart");
       }
-      for (let i = 0; i < contentTitle.length; i++) {
-        if (contentTitle[i].isAccessory) {
-          console.log(contentTitle[i]);
-          containerAccessories.appendChild(
-            dynamicClothingSection(contentTitle[i])
-          );
-        } else {
-          console.log(contentTitle[i]);
-          containerClothing.appendChild(
-            dynamicClothingSection(contentTitle[i])
-          );
-        }
-      }
+    });
+
+    boxDiv.appendChild(boxLink);
+    boxLink.appendChild(imgTag);
+    boxLink.appendChild(detailsDiv);
+    detailsDiv.appendChild(h3);
+    detailsDiv.appendChild(h4);
+    detailsDiv.appendChild(h2);
+    detailsDiv.appendChild(add_to_cart);
+
+    if (product.isAccessory) {
+      ContainerAccessories.appendChild(boxDiv);
     } else {
-      console.log("call failed!");
+      ContainerClothing.appendChild(boxDiv);
+    }
+  });
+}
+
+function checkDuplicate(product) {
+  for (let i = 0; i < CartArr.length; i++) {
+    if (CartArr[i].id === product.id) {
+      return true;
     }
   }
-};
-httpRequest.open(
-  "GET",
-  "https://648050cdf061e6ec4d490d80.mockapi.io/product",
-  true
-);
-httpRequest.send();
+  return false;
+}
