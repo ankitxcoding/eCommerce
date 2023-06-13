2db5b4595703178e1f2366520203de96a3db338b
ankitxcoding <ankitxcoding@gmail.com>
2023-06-13 15:50:16 +0530

    wishlist js added

---
+++ script/wishlist.js
@@ -0,0 +1,37 @@
+let CartArr = JSON.parse(localStorage.getItem("cart")) || [];
+let CartList = document.getElementById("cart-list");
+let ClearCartBtn = document.getElementById("clear-cart");
+
+function updateCart() {
+  CartList.innerHTML = "";
+  CartArr.forEach((item) => {
+    let cartItem = document.createElement("li");
+    let productImage = document.createElement("img");
+    let productInfo = document.createElement("div");
+    let productName = document.createElement("h4");
+    let productPrice = document.createElement("h4");
+    let productQuantity = document.createElement("h4");
+
+    productImage.src = item.preview;
+    productName.textContent = item.name;
+    productPrice.textContent = `Price: Rs. ${item.price}`;
+    productQuantity.textContent = `Quantity: ${item.quantity}`;
+
+    cartItem.appendChild(productImage);
+    productInfo.appendChild(productName);
+    productInfo.appendChild(productPrice);
+    productInfo.appendChild(productQuantity);
+    cartItem.appendChild(productInfo);
+    CartList.appendChild(cartItem);
+  });
+}
+
+function clearCart() {
+  CartArr = [];
+  localStorage.removeItem("cart");
+  updateCart();
+}
+
+ClearCartBtn.addEventListener("click", clearCart);
+
+updateCart();
